d4a49003dc44870509c459b8f7b0496febaa445f
jazevedo620 <joseph.az@gatech.edu>
2019-09-14 06:55:17 -0400

    Add automation script

---
+++ src/checkstyle.py
@@ -0,0 +1,113 @@
+# Dependency check
+try:
+    # pylint: disable=unused-import
+    import pylint
+except ImportError:
+    print(
+        """Error, Module pylint is required
+********************************
+It can be installed by running:
+
+  pip install pylint
+""")
+
+import os
+import argparse
+from pylint import epylint as lint
+
+__author__ = "CS 2340 TAs"
+__version__ = "1.0"
+
+"""
+Checkstyle script to automatically run pylint on every python source file within
+the current working directory (using class-specific options)
+"""
+
+DESCRIPTION = "Checkstyle script to run pylint on every .py file in the CWD"
+DISABLED_CHECKS = ['missing-docstring', 'no-else-return']
+BASE_OPTIONS = "--const-naming-style=any"
+PYTHON_EXTENSION = ".py"
+
+
+def main(root=None, verbose=False, process_count=None):
+    """
+    Runs the main pylint script and parses/redirects output
+    """
+
+    args = []
+    if process_count is not None:
+        args.append("-j {}".format(process_count))
+
+    path = os.path.abspath(root) if root is not None else os.getcwd()
+    files = find_files(path, PYTHON_EXTENSION)
+    output = run_linter(files, " ".join(args))
+    print()
+    print("Running pylint on {} files:".format(len(files)))
+    # Print each file in verbose mode
+    if verbose:
+        for file in files:
+            print(" - {}".format(file))
+    print()
+    print(output.getvalue())
+
+
+def run_linter(files, args):
+    """
+    Runs pylint on every file specified using the class-specific
+    arguments as well as any additional ones specified
+
+    Parameters:
+    files (array(string)): filepaths to python source files
+    args (string): CLI arguments
+
+    Returns:
+    the stdout output from pylint
+    """
+
+    if not files:
+        return ""
+
+    command = "{} {}".format(" ".join(files), get_options(args))
+    (pylint_stdout, _) = lint.py_run(command, return_std=True)
+    return pylint_stdout
+
+
+def find_files(path, extension):
+    """
+    Gets a list of every file in the given path that has the given file extension
+    """
+
+    file_list = []
+    for root, _, files in os.walk(path):
+        for file in files:
+            if file.endswith(extension):
+                file_list.append(os.path.join(root, file))
+
+    return file_list
+
+
+def get_options(additional_options):
+    """
+    Formats the options string
+
+    Parameters:
+    additional_options (string): additional arguments passed to the CLI to append
+    """
+
+    return "--disable={} {} {}".format(",".join(DISABLED_CHECKS), BASE_OPTIONS, additional_options)
+
+
+# Run script
+if __name__ == "__main__":
+    # Argument definitions
+    parser = argparse.ArgumentParser(description=DESCRIPTION)
+    parser.add_argument('--root', '-r', metavar='path',
+                        help='the path to run pylint over (defaults to current working directory)')
+    parser.add_argument('--parallel', '-p', '-j', metavar='count',
+                        help='the number of parallel processes to split pylint into')
+    parser.add_argument('--verbose', '-v', action='store_true',
+                        help='whether to display additional output')
+
+    # Parse arguments
+    parsed_args = parser.parse_args()
+    main(root=parsed_args.root, process_count=parsed_args.parallel, verbose=parsed_args.verbose)
--- test/flask/api.py
+++ test/flask/api.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_restful import Api
-from models import Object
+from models.object import Object
 
 
 app = Flask(__name__)
