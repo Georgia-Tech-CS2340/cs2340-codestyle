45510c58416aed05950725cdeb25b18225258a2e
jazevedo620 <joseph.az@gatech.edu>
2019-09-16 19:10:42 -0400

    Finalize gitignore code; fix codestyle

--- run_checkstyle.py
+++ run_checkstyle.py
@@ -5,10 +5,10 @@ the current working directory (using class-specific options)
 
 import os
 import argparse
-import subprocess
 import urllib.request
 import sys
 import re
+import subprocess
 from subprocess import PIPE
 from shutil import which
 
@@ -17,12 +17,15 @@ __version__ = "1.0"
 
 DESCRIPTION = "Checkstyle script to run checkstyle on every .java file in the CWD"
 JAVA_EXTENSION = ".java"
-CHECKSTYLE_JAR_NAME = "checkstyle-8.24-all.jar"
-CHECKSTYLE_JAR_URL = "https://github.com/checkstyle/checkstyle/releases/download/checkstyle-8.24/checkstyle-8.24-all.jar"
-CHECKSTYLE_JAR_GITIGNORE = "checkstyle-*.jar"
-CHECKSTYLE_XML_NAME = "cs2340_checks.xml"
-CHECKSTYLE_XML_URL = "https://raw.githubusercontent.com/jazevedo620/cs2340-codestyle/master/cs2340_checks.xml"
 BASE_PROCESS = ["java", "-jar"]
+CHECKSTYLE_JAR_NAME = "checkstyle-8.24-all.jar"
+CHECKSTYLE_JAR_URL = "https://github.com/checkstyle/checkstyle/releases/download/checkstyle-8.24/checkstyle-8.24-all.jar" # pylint: disable=line-too-long
+CHECKSTYLE_JAR_GITIGNORE = "checkstyle-*.jar"
+CHECKSTYLE_JAR_PATTERN = "checkstyle-.*\.jar"
+CHECKSTYLE_XML_NAME = "cs2340_checks.xml"
+CHECKSTYLE_XML_URL = "https://raw.githubusercontent.com/jazevedo620/cs2340-codestyle/master/cs2340_checks.xml" # pylint: disable=line-too-long
+
+# Scoring
 MULTILINE_COMMENT_REGEX = r"\/\*([\S\s]+?)\*\/"
 SINGLELINE_COMMENT_REGEX = r"\/{2,}.+"
 STATEMENT_REGEX = r"[^;]+?;"
@@ -30,25 +33,38 @@ AFTER_BRACE_REGEX = r"{\s*;"
 EMPTY_STATEMENT_REGEX = r"^\s*;"
 CHECKSTYLE_OUTPUT_REGEX = r"^\[[A-Z]+\] (.+?):\d+(?::\d+?)?:"
 SCORE_FORMAT = """Your code has been rated at {:.2f}/10 [raw score: {:.2f}/10]"""
+
+# Gitignore analysis
 GIT_REPO_COMMAND = ["git", "rev-parse", "--show-toplevel"]
 GIT_REPO_FAILED_SUBSTRING = "fatal: not a git repository"
 GITIGNORE = ".gitignore"
 IGNORED_FILE_COMMAND = ["git", "status", "--ignored", "--short"]
 IGNORED_FILE_REGEX = "^!! (.+)$"
 
+# Patterns
+NO_JAVA_TEXT = """
+Java is required to run Checkstyle. Make sure it is installed
+and that it exists on your PATH. More instructions are available here:
 
-def main(root=None, verbose=False, strict=False):
+https://www.java.com/en/download/help/path.xml"""
+ADDED_GITIGNORE_TEXT = """
+> Note: The checkstyle jar has been downloaded and a gitignore has automatically been created
+        for you at the project root. This file should be checked into version control.
+""".lstrip()
+MODIFIED_GITIGNORE_TEXT = """
+> Note: The checkstyle jar has been downloaded and automatically added to your gitignore.
+        This change should be checked into version control.
+""".lstrip()
+
+
+def main(root=None, verbose=False):
     """
     Runs the main checkstyle script and parses/redirects output
     """
 
     # Verify java is installed
     if which("java") is None:
-        print(
-            """Java is required to run Checkstyle. Make sure it is installed
-and that it exists on your PATH. More instructions are available here:
-
-https://www.java.com/en/download/help/path.xml""")
+        print(NO_JAVA_TEXT)
         return
 
     # Assemble dependencies
@@ -56,10 +72,9 @@ https://www.java.com/en/download/help/path.xml""")
     xml_path, _ = find_or_download(CHECKSTYLE_XML_NAME, CHECKSTYLE_XML_URL)
     jar_path, jar_downloaded = find_or_download(CHECKSTYLE_JAR_NAME, CHECKSTYLE_JAR_URL)
     if jar_downloaded:
-        if add_to_gitignore(jar_path):
-            print("""> Note: The checkstyle jar has been downloaded and automatically added to your gitignore.
-        This change should be checked into version control.""")
-            print()
+        result, mode = add_to_gitignore(jar_path)
+        if result:
+            print(ADDED_GITIGNORE_TEXT if mode == "add" else MODIFIED_GITIGNORE_TEXT)
 
     path = os.path.abspath(root) if root is not None else os.getcwd()
     files = find_files(path, JAVA_EXTENSION)
@@ -70,8 +85,7 @@ https://www.java.com/en/download/help/path.xml""")
         for file in files:
             print(" - {}".format(file))
 
-    output = run_checkstyle(files, jar_path=jar_path,
-                            xml_path=xml_path, strict=strict)
+    output = run_checkstyle(files, jar_path=jar_path, xml_path=xml_path)
     print()
     print(output)
 
@@ -89,35 +103,35 @@ def add_to_gitignore(filename):
     """
 
     if which("git") is None:
-        return False
+        return False, ""
 
     repo_relative_folder = os.path.dirname(filename)
     result = subprocess.run(GIT_REPO_COMMAND, stdout=PIPE, cwd=repo_relative_folder)
     repo_root = result.stdout.decode(sys.stdout.encoding).strip()
 
     if GIT_REPO_FAILED_SUBSTRING in repo_root:
-        return False
+        return False, ""
 
     # Clean path
     repo_root = os.path.normpath(repo_root)
 
     # Find .gitignore file if it exists by walking down the tree
     gitignore_path = find_gitignore(repo_root, repo_relative_folder)
-    to_ignore_name = CHECKSTYLE_JAR_GITIGNORE
-    print(to_ignore_name)
+
+    # Do nothing
+    if is_ignored(CHECKSTYLE_JAR_PATTERN, cwd=repo_relative_folder):
+        return False, ""
 
     if gitignore_path is None:
         # Create gitignore file at project root
         with open(os.path.join(repo_root, GITIGNORE), "w") as gitignore:
-            gitignore.write(to_ignore_name)
-        return True
-    else:
-        if is_ignored(to_ignore_name, cwd=repo_relative_folder):
-            return False
-        else:
-            with open(gitignore_path, "a") as gitignore:
-                gitignore.write(to_ignore_name)
-            return True
+            gitignore.write(CHECKSTYLE_JAR_GITIGNORE + "\n")
+        return True, "add"
+
+    # Append to existing
+    with open(gitignore_path, "a") as gitignore:
+        gitignore.write(CHECKSTYLE_JAR_GITIGNORE + "\n")
+    return True, "modify"
 
 
 def find_gitignore(repo_root, top_folder):
@@ -126,14 +140,19 @@ def find_gitignore(repo_root, top_folder):
     the tree until the repo_root is found, or None if not found
     """
 
-    # TODO implement
-    print(repo_root)
-    print(top_folder)
+    repo_root = os.path.normpath(repo_root)
+    current_path = os.path.normpath(top_folder)
+    while len(current_path) >= len(repo_root):
+        gitignore_path = os.path.join(current_path, GITIGNORE)
+        if os.path.exists(gitignore_path):
+            return gitignore_path
+
+        current_path = os.path.dirname(current_path)
 
     return None
 
 
-def is_ignored(filename, cwd=None):
+def is_ignored(regex, cwd=None):
     """
     Determines whether the given file is ignored in its containing git repository
     """
@@ -141,8 +160,11 @@ def is_ignored(filename, cwd=None):
     result = subprocess.run(IGNORED_FILE_COMMAND, stdout=PIPE, cwd=cwd)
     output = result.stdout.decode(sys.stdout.encoding)
     for line in output.splitlines():
-        if re.match(IGNORED_FILE_REGEX, line) and filename in line:
-            return True
+        match = re.search(IGNORED_FILE_REGEX, line)
+        if match:
+            line_filename = match.group(1)
+            if re.search(regex, line_filename):
+                return True
 
     return False
 
@@ -157,7 +179,7 @@ def find_or_download(filename, url):
     target_path = os.path.join(current_dir, filename)
     exists = os.path.exists(target_path)
 
-    if (exists):
+    if exists:
         return target_path, False
 
     # Download file
@@ -171,14 +193,14 @@ def assemble_score(files, checkstyle_output):
     """
     Calculates code "score" using the same formula as pylint for consistency:
     https://docs.pylint.org/en/1.6.0/faq.html#pylint-gave-my-code-a-negative-rating-out-of-ten-that-can-t-be-right
-    """
+    """  # pylint: disable=line-too-long
 
     errors = count_errors(checkstyle_output)
     statements = 0
     for filename in files:
         statements = count_statements(filename)
 
-    if statements is 0:
+    if statements == 0:
         return 10.0
 
     return 10.0 - ((float(5 * errors) / statements) * 10)
@@ -210,13 +232,13 @@ def count_statements(filename):
 
     count = 0
     with open(filename, "r+") as java_file:
-        buffer = ""
         contents = java_file.read()
         contents = re.sub(MULTILINE_COMMENT_REGEX, "", contents)
         contents = re.sub(SINGLELINE_COMMENT_REGEX, "", contents)
         for match in re.finditer(STATEMENT_REGEX, contents):
             statement_candidate = match.group()
-            if not re.search(AFTER_BRACE_REGEX, buffer) and not re.search(EMPTY_STATEMENT_REGEX, buffer):
+            if not re.search(AFTER_BRACE_REGEX, statement_candidate) and not re.search(
+                    EMPTY_STATEMENT_REGEX, statement_candidate):
                 count += 1
 
     return count
@@ -232,16 +254,16 @@ def reporthook(blocknum, blocksize, totalsize):
     readsofar = blocknum * blocksize
     if totalsize > 0:
         percent = readsofar * 1e2 / totalsize
-        s = "\r%5.1f%% %*d / %d" % (
+        progress = "\r%5.1f%% %*d / %d" % (
             percent, len(str(totalsize)), readsofar, totalsize)
-        sys.stdout.write(s)
+        sys.stdout.write(progress)
         if readsofar >= totalsize:
             sys.stdout.write("\n")
     else:
         sys.stdout.write("read %d\n" % (readsofar,))
 
 
-def run_checkstyle(files, jar_path=None, xml_path=None, strict=False):
+def run_checkstyle(files, jar_path=None, xml_path=None):
     """
     Runs checkstyle on every file specified using the class-specific
     arguments as well as any additional ones specified
@@ -250,7 +272,6 @@ def run_checkstyle(files, jar_path=None, xml_path=None, strict=False):
     files (array(string)): filepaths to java source files
 
     Named:
-    strict (boolean): Whether to run the linter in strict mode
     jar_path (string): The filepath to the checkstyle jar
     xml_path (string): The filepath to the checkstyle config XML file
 
@@ -288,15 +309,13 @@ def bootstrap():
     # Argument definitions
     parser = argparse.ArgumentParser(description=DESCRIPTION)
     parser.add_argument("--root", "-r", metavar="path",
-                        help="the path to run checkstyle over (defaults to current working directory)")
+                        help="the path to run checkstyle over (defaults to current directory)")
     parser.add_argument("--verbose", "-v", action="store_true",
                         help="whether to display additional output")
-    parser.add_argument("--all", "-a", "--strict", action="store_true",
-                        help="enables all checks (strict mode)")
 
     # Parse arguments
     parsed_args = parser.parse_args()
-    main(root=parsed_args.root, verbose=parsed_args.verbose, strict=parsed_args.all)
+    main(root=parsed_args.root, verbose=parsed_args.verbose)
 
 
 # Run script
